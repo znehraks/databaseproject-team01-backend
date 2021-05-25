cde9f291de6c73b5edc1ce28099ea22eb1101c7e
designC <znehraks@gmail.com>
2021-05-25 17:01:41 +0900

    semi-complete

--- router/req05/index.js
+++ router/req05/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const connection = require("../../connection");
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("/:emp_no", (req, res) => {
   let sql = `SELECT * FROM client C INNER JOIN project P ON C.client_no = P.client_no
     INNER JOIN project_position PP ON P.project_no = PP.project_no
     WHERE role_no = 1 AND emp_no = ${req.params.emp_no};`;
